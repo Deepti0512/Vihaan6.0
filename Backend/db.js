8772a5992b6fcae8e7738b73c3c06a092a54e583
Deepti <deeptimittal0512@gmail.com>
2023-04-27 23:17:15 +0530

    update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
  
 const connectionUrl = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.fvur2bk.mongodb.net/test`
-console.log(process.env.PORT);
+
 module.exports = async function connectDb() {
   await mongoose
+    .connect(connectionUrl)
     .then(() => console.log("Connected to database"))
     .catch((err) => console.log(err));
 };
